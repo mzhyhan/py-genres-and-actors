c4b03babb51e97875af905a986b1bb1b03564f3a
Mykhaylo Zhyhan <mz5157e@gre.ac.uk>
2026-02-08 20:31:29 +0000

    created tables Actor and Genre, used commands like create, delete and updated some rows

--- main.py
+++ main.py
@@ -9,31 +9,35 @@ from db.models import Actor
 def main() -> QuerySet:
     Actor.objects.all().delete()
     Genre.objects.all().delete()
+    genres = (["Western", "Action", "Dramma"])
+    names = [
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson")
+    ]
+    for genre in genres:
+        Genre.objects.create(name=genre)
 
-    Genre.objects.create(name="Western")
-    action = Genre.objects.create(name="Action")
-    drama = Genre.objects.create(name="Dramma")
+    for first, last in names:
+        Actor.objects.create(first_name=first, last_name=last)
 
-    george = Actor.objects.create(first_name="George", last_name="Klooney")
-    kianu = Actor.objects.create(first_name="Kianu", last_name="Reaves")
-    Actor.objects.create(first_name="Scarlett", last_name="Keegan")
-    Actor.objects.create(first_name="Will", last_name="Smith")
-    Actor.objects.create(first_name="Jaden", last_name="Smith")
-    Actor.objects.create(first_name="Scarlett", last_name="Johansson")
-
+    drama = Genre.objects.get(name="Dramma")
     drama.name = "Drama"
     drama.save()
+    george = Actor.objects.get(first_name="George", last_name="Klooney")
+    george.first_name = "George"
     george.last_name = "Clooney"
     george.save()
+    kianu = Actor.objects.get(first_name="Kianu", last_name="Reaves")
     kianu.first_name = "Keanu"
     kianu.last_name = "Reeves"
     kianu.save()
-
-    action.delete()
+    Genre.objects.filter(name="Action").delete()
     Actor.objects.filter(first_name="Scarlett").delete()
 
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
 
-
-if __name__ == "__main__":
-    main()
+print(main())
